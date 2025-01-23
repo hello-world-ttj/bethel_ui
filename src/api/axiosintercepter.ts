8a7f11d0c5404fad5e6b14768dd0a85887d86de7
Anjana V U <103420152+anjanavu@users.noreply.github.com>
2025-01-23 23:40:03 +0530

    #user,church

---
+++ src/api/axiosintercepter.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+// Declare the environment variables for TypeScript
+declare global {
+  interface ImportMetaEnv {
+    readonly VITE_APP_API_URL: string;
+    readonly VITE_API_KEY: string;
+    readonly VITE_AUTH_TOKEN: string;
+  }
+
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+}
+
+const baseURL =
+  import.meta.env.VITE_APP_API_URL 
+
+const axiosInstance = axios.create({
+  baseURL: baseURL,
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token =
+      import.meta.env.VITE_AUTH_TOKEN 
+    const apiKey = import.meta.env.VITE_API_KEY 
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    if (apiKey) {
+      config.headers["api-key"] = apiKey;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
