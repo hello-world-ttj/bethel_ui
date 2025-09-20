844800e4870a9bd05dd1a1a98fc7f75588db98e5
Anjana V U <103420152+anjanavu@users.noreply.github.com>
2025-09-20 14:00:53 +0530

    feat:update expiry

--- src/api/subscriptionApi.ts
+++ src/api/subscriptionApi.ts
@@ -43,7 +43,7 @@ export const updateSubscription = async (
     const response = await axiosInstance.put(`/subscription/${id}`, data);
     toast.success(response.data.message);
     return response.data;
-  } catch (error : any) {
+  } catch (error: any) {
     throw error.response.data;
   }
 };
@@ -52,7 +52,7 @@ export const deleteSubscription = async (id: string): Promise<any | null> => {
     const response = await axiosInstance.delete(`/subscription/${id}`);
     toast.success(response.data.message);
     return response.data;
-  } catch (error : any) {
+  } catch (error: any) {
     throw error.response.data;
   }
 };
@@ -65,12 +65,23 @@ export const getPdf = async (): Promise<any | null> => {
     return null;
   }
 };
-export const getSubscriptionByUserId = async (id: string): Promise<any | null> => {
+export const getSubscriptionByUserId = async (
+  id: string
+): Promise<any | null> => {
   try {
     const response = await axiosInstance.get(`/subscription/user/${id}`);
     return response.data;
   } catch (error) {
     console.error("Failed to fetch subscription:", error);
+    return null;
+  }
+};
+export const updateExpiry = async (): Promise<any | null> => {
+  try {
+    const response = await axiosInstance.patch(`/subscription/update/expiry`);
+    return response.data;
+  } catch (error) {
+    console.error("Failed to fetch members:", error);
     return null;
   }
 };
